22026ac840b1c54161db7e5e4058d0f93ac3c619
Christian Gesell <christian.gesell@t-online.de>
2017-04-23 16:37:00 +0200

    cpplint.py

--- bankingApplication/BankApplication.cpp
+++ bankingApplication/BankApplication.cpp
@@ -3,23 +3,23 @@
 
 using namespace std;
 
-BankApplication::BankApplication(IAccountFactory * accountFactory) : accountFactory(accountFactory) {
-}
+BankApplication::BankApplication(IAccountFactory* accountFactory)
+    : accountFactory(accountFactory) {}
 
 void BankApplication::run() {
-    cout << "Welcome to our Bank!" << endl;
-    cout << endl;
+  cout << "Welcome to our Bank!" << endl;
+  cout << endl;
 
-    accounts.push_back(accountFactory->make("WellnessAccount"));
-    accounts.push_back(accountFactory->make("CrazyAccount"));
+  accounts.push_back(accountFactory->make("WellnessAccount"));
+  accounts.push_back(accountFactory->make("CrazyAccount"));
 
-    cout << "Active accounts:" << endl;
-    for (auto account : accounts) {
-        cout << "- " << account->advertise() << endl;
-    }
-    cout << endl;
+  cout << "Active accounts:" << endl;
+  for (auto account : accounts) {
+    cout << "- " << account->advertise() << endl;
+  }
+  cout << endl;
 
-    cout << "Available account types:" << endl;
-    for (const auto name : accountFactory->getAccountNames())
-        cout << "- " << name << endl;
+  cout << "Available account types:" << endl;
+  for (const auto name : accountFactory->getAccountNames())
+    cout << "- " << name << endl;
 }
